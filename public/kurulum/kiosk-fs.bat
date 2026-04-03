0c05d3b32ffcae9821c488e7a2b70f27750da4ed
Yasin Gülsoy <yasingulsoy02@gmail.com>
2026-04-03 14:33:13 +0300

    Update kiosk-fs.bat script to include internet connectivity check; enhance startup process with retry logic for internet access before launching the browser in kiosk mode.

--- public/kurulum/kiosk-fs.bat
+++ public/kurulum/kiosk-fs.bat
@@ -1,14 +1,24 @@
 @echo off
 REM HospiSync Signage - Kiosk Baslatici
-REM Bu dosya oturum acildiginda otomatik calisir
+REM Internet yoksa 5 dakikada bir tekrar dener
 
 taskkill /F /IM chrome.exe >nul 2>&1
 taskkill /F /IM msedge.exe >nul 2>&1
 
-timeout /t 8 /nobreak >nul
-
 set "URL=https://hospisync.cloud/fs"
 
+REM Ilk acilista ag icin 10 sn bekle
+timeout /t 10 /nobreak >nul
+
+:BEKLE
+ping -n 1 -w 3000 8.8.8.8 >nul 2>&1
+if errorlevel 1 (
+    echo [%date% %time%] Internet yok, 5 dk sonra tekrar deneniyor...
+    timeout /t 300 /nobreak >nul
+    goto BEKLE
+)
+
+REM Internet var, tarayiciyi ac
 if exist "C:\Program Files\Google\Chrome\Application\chrome.exe" (
     start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --kiosk --disable-infobars --disable-session-crashed-bubble --noerrdialogs --disable-translate --no-first-run --autoplay-policy=no-user-gesture-required --start-fullscreen "%URL%"
     goto :eof
